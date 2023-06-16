acf7aa865e0dfd972e8bdc859797bb2fa3d93d47
XxthyagoronaldxX <thyago.silva@netbiis.com>
2023-06-16 14:46:01 -0300

    First Commit

---
+++ main.pl
@@ -0,0 +1,38 @@
+% OBJETOS:
+% 
+% Luzes em cada cômodo. 
+% SmartTV apenas na sala de estar.  
+% Ar-condicionado nos 3 quartos e na sala de estar. 
+% Cortinas motorizadas em todos os cômodos.
+% Computador nos 3 quartos.
+% Aparelho de som na sala e no quarto 1. 
+% Geladeira e fogão na cozinha.
+% Fechadura inteligente nas portas e janelas.
+
+
+% USECASES:
+%
+% LISTAR OBJETOS QUE ESTAO LIGADOS E DESLIGADOS.
+% INFERIR QUE O USUARIO ESTA DORMINDO OU ACORDADO. (COM BASE NO ESTADO DOS OBJETOS)
+% SKILL DORMIR.
+% SKILL FORA DE CASA.
+% SKILL GAMER.
+% SKILL FAXINA.
+
+% POSSIVEL IDEAI:
+%
+% PERSONALIZAÇAO DE SKILL.
+% PERSONALIZAÇAO DE COMODO DA CASA.
+% INTERAGIR DIRETAMENTE COM O AGENTE INTELIGENTE. (Que dia e hoje? Que horas sao? Vai chover hoje?)
+% SKILL DE HABILIDADES DO AGENTE INTELIGENTE.
+% ESCUTAR EVENTO DE OBJETOS. (DEFINIR METRICAS)			
+
+
+
+evento().
+
+estado().
+
+acao().
+
+skill().
